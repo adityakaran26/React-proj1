7647c0c41d8074b231eaac0cc317a729a3b508a9
Aditya singh <adityasingh@Adityas-MacBook-Air.local>
2026-05-31 22:18:27 +0530

    text update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ const Hero = () => {
         <>
         <div className="hero-comp">
             <div className="hero-text">
-                <h1>YOUR FEET DESERVE THE BEST</h1>
+                <h1>YOUR FEET DESERVE THE BEST!!</h1>
                 <h3>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</h3>
                 <div className="btn-wrap"><button className="cta-button">Shop Now!</button></div>
                 <div className="btn-wrap"><button className="cta-button">Category</button></div>
